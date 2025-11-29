5c43826efdc758ff1bb7e94e06a15e84618c1003
likunrui2 <likunrui2@163.com>
2025-11-29 23:42:57 +0800

    雪糕tv测试

---
+++ PgitvProvider/src/main/kotlin/com/pgitv/PgiTvPlugin.kt
@@ -0,0 +1,18 @@
+package com.pgitv
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class PgiTvPlugin: Plugin() {
+    private var activity: AppCompatActivity? = null
+
+    override fun load(context: Context) {
+        activity = context as? AppCompatActivity
+
+        // All providers should be added in this manner
+        registerMainAPI(PgiTvProvider())
+    }
+}
---
+++ PgitvProvider/src/main/kotlin/com/pgitv/PgiTvProvider.kt
@@ -0,0 +1,46 @@
+package com.pgitv
+
+import com.lagradost.cloudstream3.LiveSearchResponse
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.MovieSearchResponse
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.newLiveSearchResponse
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import org.jsoup.nodes.Element
+
+class PgiTvProvider : MainAPI() {
+    override var mainUrl = "https://www.pgitv.com/"
+    override var name = "雪糕TV"
+    override val supportedTypes = setOf(TvType.Movie)
+
+    override var lang = "zh"
+
+    // Enable this when your provider has a main page
+    override val hasMainPage = true
+
+    // This function gets called when you search for something
+    override suspend fun search(query: String): List<SearchResponse> {
+        val document =
+            app.get(
+                "$mainUrl/vsearch/-------------.html",
+                params = mapOf("wd" to query),
+                referer = mainUrl
+            ).document
+        return document.select("ul.list-show-all li").map { it.toMovieSearchResponse() }
+    }
+
+    private fun Element.toMovieSearchResponse(): MovieSearchResponse {
+        val anchor = this.select("a")
+        val linkName = anchor.attr("href").substringAfterLast("/")
+        val name = anchor.firstOrNull { it.text().isNotBlank() }?.text()
+        val image = this.select("img").attr("src")
+        return newMovieSearchResponse(
+            name ?: "",
+            linkName,
+            TvType.Live,
+            fix = false
+        ) { posterUrl = image }
+    }
+}
